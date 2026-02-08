7004f3296edc0d205b4bbe39820f3e297fe49071
Kis319 <wafflem19@gmail.com>
2026-02-08 19:58:15 +0900

    CSSとJSに要素を分割。文字情報はcontentで管理するように変更。

---
+++ js/content.js
@@ -0,0 +1,7 @@
+const siteContent = {
+  title: "Peskei's Server",
+  descriptionHtml:
+    "このサーバーでは <strong>Minecraft</strong> と <strong>Golf It!</strong> を中心に、みんなでマルチプレイを楽しめます。",
+  gameTags: ["Minecraft", "Golf It!", "and more..."],
+  footer: "© 2026 Peskei's Server. Hosted on GitHub Pages."
+};
